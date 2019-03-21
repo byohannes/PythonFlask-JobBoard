4be337e3971af9096f4d9944fe4dc3834431df00
byohannes <42332389+byohannes@users.noreply.github.com>
2019-03-21 21:30:52 +0000

    completed module

--- jobs/app.py
+++ jobs/app.py
@@ -13,13 +13,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
-        results = connection.commit
+        results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
 
     cursor.close()
     return results
 @app.teardown_appcontext
+
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
